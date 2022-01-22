47ba8107c8dc8f132e0f5ed77c2913354ba6563f
jsprofessional <jshuman@protonmail.com>
2022-01-22 15:05:39 -0600

    Added real routes to hrefs. Added some styling. Changed wording to reflect Bangkok Inn and design credits

--- bangkok_inn/bangkok_inn/urls.py
+++ bangkok_inn/bangkok_inn/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/app/')),
     path('app/', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
